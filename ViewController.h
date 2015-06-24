a66e2b591a5be635b7b7c5610ce6a003c2897155
xuweijie_18696781905 <151611438@qq.com>
2015-06-24 09:48:26 +0800

    init
    init

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXCircleSlider
+//
+//  Created by JackXu on 15/6/23.
+//  Copyright (c) 2015年 BFMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JXCircleSlider
+//
+//  Created by JackXu on 15/6/23.
+//  Copyright (c) 2015年 BFMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
